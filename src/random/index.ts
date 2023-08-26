d551ed19b234f9c8b702209d0ddfc3ee904ebe70
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-25 23:17:02 -0700

    WIP: making color/

--- src/random/index.ts
+++ src/random/index.ts
@@ -1,10 +1,31 @@
 
 
 /**
- * Returns a random number between min (inclusive) and max (exclusive):
+ * Returns a random number (float) between min (inclusive) and max (exclusive):
  * [min,max)→R
  */
-function randFloat(min: number, max: number): number {
+export function randFloat(min: number, max: number): number {
     return Math.random() * (max - min) + min
 }
 
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive):
+ * [min,max]→Z
+ */
+export function randInt(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+/**
+ * Returns a random element from the given array.
+ * @param array The array from which to choose a random element.
+ * @returns A randomly selected element from the array.
+ */
+export function randChoice<T>(array: T[]): T {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
--- src/random/test.ts
+++ src/random/test.ts
@@ -1,8 +1,41 @@
-import { greet } from './index';
+import { randFloat, randInt, randChoice } from '.';
 
-describe('greet function', () => {
-    it('should return a greeting message with the provided name', () => {
-        const result = greet('Alice');
-        expect(result).toBe('Hello, Alice!');
+describe('randFloat', () => {
+    it('generates numbers within the specified range', () => {
+        const min = 0;
+        const max = 10;
+        const iterations = 1000; // Number of test iterations
+
+        for (let i = 0; i < iterations; i++) {
+            const result = randFloat(min, max);
+            expect(result).toBeGreaterThanOrEqual(min);
+            expect(result).toBeLessThan(max);
+        }
+    });
+});
+
+describe('randInt', () => {
+    it('generates integers within the specified range', () => {
+        const min = 5;
+        const max = 15;
+        const iterations = 1000; // Number of test iterations
+
+        for (let i = 0; i < iterations; i++) {
+            const result = randInt(min, max);
+            expect(result).toBeGreaterThanOrEqual(min);
+            expect(result).toBeLessThanOrEqual(max);
+        }
+    });
+});
+
+describe('randChoice', () => {
+    it('selects a random element from the array', () => {
+        const array = [1, 2, 3, 4, 5];
+        const iterations = 1000; // Number of test iterations
+
+        for (let i = 0; i < iterations; i++) {
+            const result = randChoice(array);
+            expect(array).toContain(result);
+        }
     });
 });

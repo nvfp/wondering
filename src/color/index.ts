a8b2e1ac4d1100ca4a7324484c529e53dbb9e3ac
Nicholas Valentinus <nvfastplease@gmail.com>
2023-09-14 02:05:41 -0700

    0.1.18 update

--- src/color/index.ts
+++ src/color/index.ts
@@ -39,3 +39,36 @@ export function interpolateColor(color1: string, color2: string, t: number): str
     return rgbToHex(r, g, b);
 }
 
+
+/**
+ * Trilinear interpolation between three colors based on a given factor.
+ * @param color1 The first color in hexadecimal format (should be in format "#ff0000").
+ * @param color2 The second color in hexadecimal format.
+ * @param color3 The third color in hexadecimal format.
+ * @param mid The midpoint between color1 and color3, a value between 0 and 1 exclusive.
+ * @param t The interpolation factor between 0 and 1.
+ * @returns The interpolated color in hexadecimal format.
+ */
+export function trilinearInterpolation(color1: string, color2: string, color3: string, mid: number, t: number): string {
+    let w1: number, w2: number, w3: number;
+
+    if (t < mid) {
+        w1 = (mid - t) / mid;
+        w2 = t / mid;
+        w3 = 0;
+    } else {
+        w1 = 0;
+        w2 = (1 - t) / (1 - mid);
+        w3 = (t - mid) / (1 - mid);
+    }
+
+    const [r1, g1, b1] = hexToRgb(color1);
+    const [r2, g2, b2] = hexToRgb(color2);
+    const [r3, g3, b3] = hexToRgb(color3);
+
+    const r = Math.round(r1 * w1 + r2 * w2 + r3 * w3);
+    const g = Math.round(g1 * w1 + g2 * w2 + g3 * w3);
+    const b = Math.round(b1 * w1 + b2 * w2 + b3 * w3);
+
+    return rgbToHex(r, g, b);
+}
